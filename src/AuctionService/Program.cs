269b582c021d90b1dfae27fbfd063f6c432dcdfa
Adib Waluya <50482736+adibwaluya@users.noreply.github.com>
2024-11-25 17:26:10 +0100

    added db context class

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,8 +1,15 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    // Take a connection string as its argumnent
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConntection"));
+});
 
 var app = builder.Build();
 
