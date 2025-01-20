fcfbaa9a549e96addcf923581d474bf28954ae13
Adib Waluya <50482736+adibwaluya@users.noreply.github.com>
2025-01-20 18:48:56 +0100

    feat: created seed data and DTOs

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
     // Take a connection string as its argumnent
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -17,5 +18,15 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+
+    Console.WriteLine(e);
+}
 
 app.Run();
