23f7dfa08b549998108aef289d6f76da00b5137d
Adib Waluya <50482736+adibwaluya@users.noreply.github.com>
2025-01-24 22:19:46 +0100

    Feature/auctionservice (#1)
    * added entity classes
    
    * added db context class
    
    * added db context class
    
    * feat: created seed data and DTOs
    
    * feat: updated the crud operations in AuctionsController

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,8 +1,16 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    // Take a connection string as its argumnent
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -10,5 +18,15 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+
+    Console.WriteLine(e);
+}
 
 app.Run();
