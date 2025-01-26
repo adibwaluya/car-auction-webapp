1c47d296b91816a75f1df1ddc3ba5c2961043b23
Adib Waluya <50482736+adibwaluya@users.noreply.github.com>
2025-01-26 11:24:02 +0100

    Feature/searchservice (#2)
    * feat: added skeleton for search service
    
    * feat: added db initializer, entity, and seed data
    
    * feat: added search controller
    
    * feat: added result pagination
    
    * feat: added ordering and filtering features

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+using SearchService.RequestHelpers;
+
+namespace SearchService.Controllers;
+
+[ApiController]
+[Route("api/search")]
+public class SearchController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
+    {
+        // Create query, which is going to be a page search
+        var query = DB.PagedSearch<Item, Item>();
+
+        // Search if we get a search term
+        // if we do, we're going to match it
+        if (!string.IsNullOrEmpty(searchParams.SearchTerm))
+        {
+            query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
+        }
+
+        // Sorting query based on their selection
+        query = searchParams.OrderBy switch
+        {
+            "make" => query.Sort(x => x.Ascending(a => a.Make)),
+            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+        };
+
+        // Filtering based on the selection
+        query = searchParams.FilterBy switch
+        {
+            "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6)
+                && x.AuctionEnd > DateTime.UtcNow),
+            _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+        };
+
+        // display the auctions that the user is selling
+        if (!string.IsNullOrEmpty(searchParams.Seller))
+        {
+            query.Match(x => x.Seller == searchParams.Seller);
+        }
+
+        if (!string.IsNullOrEmpty(searchParams.Winner))
+        {
+            query.Match(x => x.Winner == searchParams.Winner);
+        }
+
+        // page the results before returning
+        query.PageNumber(searchParams.PageNumber);
+        query.PageSize(searchParams.PageSize);
+
+        var result = await query.ExecuteAsync();
+
+        // pageCount = maybe the amount of pages to show the results (3 pages)
+        // totalCount = the total results, which in this case 10
+        return Ok(new
+        {
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount
+        });
+    }
+}
