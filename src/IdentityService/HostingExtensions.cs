8946115dcf8c26be55363cdbd10868007ac88846
Adib Waluya <50482736+adibwaluya@users.noreply.github.com>
2025-02-05 23:42:44 +0100

    feat: added seed data and migration for postgres config

--- src/IdentityService/HostingExtensions.cs
+++ src/IdentityService/HostingExtensions.cs
@@ -36,6 +36,12 @@ internal static class HostingExtensions
             .AddInMemoryClients(Config.Clients)
             .AddAspNetIdentity<ApplicationUser>();
 
+        builder.Services.ConfigureApplicationCookie(options =>
+        {
+            // required to get the functionality working when using http
+            options.Cookie.SameSite = SameSiteMode.Lax;
+        });
+
         builder.Services.AddAuthentication();
 
         return builder.Build();
