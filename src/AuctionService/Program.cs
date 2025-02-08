54e81a78e8b37e22ffcfd88b927c76c4adcc91b8
Adib Waluya <50482736+adibwaluya@users.noreply.github.com>
2025-02-08 23:32:40 +0100

    feat: configured the auth endpoints on the resource server

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,6 +1,7 @@
 using AuctionService.Consumers;
 using AuctionService.Data;
 using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -37,9 +38,21 @@ builder.Services.AddMassTransit(x =>
 }
 );
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        // gets/sets authority to use when making OpenID Connect calls
+        // need to match the URL of the identity server
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;       // run on http
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseAuthentication();        // must come before authorization process
 app.UseAuthorization();
 
 app.MapControllers();
