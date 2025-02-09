0239473348051fc3571cb3096c8500859a561c89
Adib Waluya <50482736+adibwaluya@users.noreply.github.com>
2025-02-09 20:31:16 +0100

    feat: added basic header

--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -1,9 +1,8 @@
 export default function Home() {
+  console.log('Server component')
   return (
     <div>
-      <h3 className='text-3xl font-semibold'>
-        Auction Web App
-      </h3>
+      Content goes here
     </div>
   );
 }
