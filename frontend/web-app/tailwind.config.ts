5bf8ea845f63cb3445fbfabe374ee8111bb0d7d4
Adib Waluya <50482736+adibwaluya@users.noreply.github.com>
2025-02-16 17:43:21 +0100

    Feature/clientappnextjs (#7)
    * feat: added basic config nextjs project
    
    * fix: simplified project
    
    * feat: added basic header
    
    * feat; implemented data fetching from API
    
    * feat: added countdown timer
    
    * feat: added loading functionality to avoid image flickering
    
    * feat: added type for parsed results
    
    * feat: added pagination and set page size option
    
    * fix: refactored to adjust zustand state management
    
    * feat; added basic layout design for search bar
    
    * feat: added search functionality and reset layout via main icon
    
    * fix: added reset search functionality
    
    * feat: added the sorting functionality
    
    * feat: added a basic component to display during zero results

---
+++ frontend/web-app/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+import flowbitePlugin from 'flowbite/plugin';
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './node_modules/flowbite-react/dist/esm/**/*.mjs',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [
+    flowbitePlugin
+  ],
+} satisfies Config;
