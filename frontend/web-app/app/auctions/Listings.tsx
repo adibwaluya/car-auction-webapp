c408ea35f81a8b07f21ecf7cf69b68a8c9c9cb3e
Adib Waluya <50482736+adibwaluya@users.noreply.github.com>
2025-02-16 17:35:41 +0100

    feat: added a basic component to display during zero results

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -9,6 +9,7 @@ import Filters from './Filters';
 import { useParamsStore } from '@/hooks/useParamsStore';
 import { useShallow } from 'zustand/shallow';
 import qs from 'query-string';
+import EmptyFilter from '../components/EmptyFilter';
 
 export default function Listings() {
     const [data, setData] = useState<PagedResult<Auction>>();
@@ -16,7 +17,8 @@ export default function Listings() {
         pageNumber: state.pageNumber,
         pageSize: state.pageSize,
         searchTerm: state.searchTerm,
-        orderBy: state.orderBy
+        orderBy: state.orderBy,
+        filterBy: state.filterBy
     })));
     const setParams = useParamsStore(state => state.setParams);
     const url = qs.stringifyUrl({url: '', query: params})
@@ -36,15 +38,20 @@ export default function Listings() {
     return (
         <>
             <Filters />
-            <div className='grid grid-cols-4 gap-6'>
-                {data.results.map(auction=> (
-                    <AuctionCard key={auction.id} auction={auction} />
-                ))}
-            </div>
-            <div className='flex justify-center mt-4'>
-                <AppPagination pageChanged={setPageNumber} currentPage={params.pageNumber} pageCount={data.pageCount} />
-            </div>
-        
+            {data.totalCount === 0 ? (
+                <EmptyFilter showReset />
+            ) : (
+                <>
+                <div className='grid grid-cols-4 gap-6'>
+                    {data.results.map(auction=> (
+                        <AuctionCard key={auction.id} auction={auction} />
+                    ))}
+                </div>
+                <div className='flex justify-center mt-4'>
+                    <AppPagination pageChanged={setPageNumber} currentPage={params.pageNumber} pageCount={data.pageCount} />
+                </div>
+                </>
+            )}
         </>
         
     )
