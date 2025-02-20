95a743114f33f5cf9dde404eb0d61b57c2cd5d9a
Adib Waluya <50482736+adibwaluya@users.noreply.github.com>
2025-02-21 00:02:31 +0100

    feat: added signalR and new bid to signalR

---
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { useAuctionStore } from '@/hooks/useAuctionStore'
+import { useBidStore } from '@/hooks/useBidStore'
+import { Bid } from '@/types'
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr'
+import { connect } from 'http2'
+import { useParams } from 'next/navigation'
+import React, { ReactNode, useCallback, useEffect, useRef } from 'react'
+
+type Props = {
+    children: ReactNode
+}
+
+export default function SignalRProvider({children}: Props) {
+    const connection = useRef<HubConnection | null>(null);
+    const setCurrentPrice = useAuctionStore(state => state.setCurrentPrice);
+    const addBid = useBidStore(state => state.addBid);
+    const params = useParams<{id: string}>();
+
+    const handleBidPlaced = useCallback((bid: Bid) => {
+        if (bid.bidStatus.includes('Accepted')) {
+            setCurrentPrice(bid.auctionId, bid.amount);
+        }
+
+        if (params.id === bid.auctionId) {
+            addBid(bid);
+        }
+    }, [setCurrentPrice, addBid, params.id]);
+
+    useEffect(() => {
+        if (!connection.current) {
+            connection.current = new HubConnectionBuilder()
+                .withUrl('http://localhost:6001/notifications')
+                .withAutomaticReconnect()
+                .build();
+
+            connection.current.start()
+                .then(() => 'Connected to notification hub')
+                .catch(err => console.log(err));
+        }
+        
+        connection.current.on('BidPlaced', handleBidPlaced)
+
+        return () => {
+            connection.current?.off('BidPlaced', handleBidPlaced)
+        }
+
+    }, [setCurrentPrice, handleBidPlaced])
+
+    return (
+        children
+    )
+}
